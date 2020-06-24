193bf70e1f1c876c0b55d58714c8cd7c583c0324
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-06-24 23:47:38 +0930

    env example

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose").set('debug', true);
-const path = require("path");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3003;
 
-const db = require("./models");
+
 
 const app = express();
 
